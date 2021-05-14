30035c7509b7137993971a5e862f4bcc13f62cd1
fgl27 <fglfgl27@gmail.com>
2021-05-14 04:15:12 -0300

    Atualiza o menu
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- test_app/react_src/index.js
+++ test_app/react_src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Text, View} from 'react-native';
 
 function login() {
@@ -32,10 +33,54 @@ const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="login" component={login} />
-      <Tab.Screen name="cadastro" component={cadastro} />
-      <Tab.Screen name="itens" component={itens} />
+    <Tab.Navigator
+      initialRouteName="login"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="login"
+        component={login}
+        options={{
+          tabBarLabel: 'Login',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="account-box"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="cadastro"
+        component={cadastro}
+        options={{
+          tabBarLabel: 'Cadastro',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="account-plus"
+              color={color}
+              size={size}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="itens"
+        component={itens}
+        options={{
+          tabBarLabel: 'Itens',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="format-list-text"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
