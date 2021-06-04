54398608340b97fde1cd44f75e2402ccea4bd2fd
larciu <laerciodasilvapedrosa@gmail.com>
2021-06-03 23:19:29 -0300

    Okay

--- app/pages/pessoa/Home.js
+++ app/pages/pessoa/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, View, Text, FlatList, Image} from 'react-native';
 
+import style from '../../../style/pessoa/Home';
+
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
@@ -17,12 +19,32 @@ const DATA = [
     title: 'Inchado',
     photo: require('../../../assets/images/logo.png')
   },
+
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d73',
+    title: 'Inchado',
+    photo: require('../../../assets/images/logo.png')
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d74',
+    title: 'Inchado',
+    photo: require('../../../assets/images/logo.png')
+  },
+
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d75',
+    title: 'Inchado',
+    photo: require('../../../assets/images/logo.png')
+  }
 ];
 
 const Item = ({ title, photo }) => (
-  <ScrollView>
-    <Image style={{width: 50, height:50}} source={photo} />
-    <Text>{title}</Text>
+  <ScrollView style={style.scroll}>
+    <View style={style.container}>
+      <Image style={style.img} source={photo} />
+      <Text style={style.textos}>{title}</Text>
+    </View>
+    <Text><Text style={{fontWeight: 'bold'}}>Descrição:</Text> Melhor do mundofjakldlasjdjdaskdjkasldjlksjdlsajdkdjklsajdksajdkajdklasjldjaskdjklsadjlkajdlsdjaskdjaksdjsakldjaslkdjalskdjasjkd</Text>
   </ScrollView>
 );
 
@@ -32,7 +54,7 @@ const renderItem = ({ item }) => (
 
 const Home: () => Node = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={style.super}>
           <FlatList keyExtractor={(item) => item.id} renderItem={renderItem} data={DATA} />      
     </SafeAreaView>
   );
---
+++ style/pessoa/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 3,
+        flexDirection: 'row'
+    },
+
+    super:{
+        marginTop: 0
+    },
+
+    scroll:{
+        marginBottom: 1,
+        padding: 16,
+        backgroundColor: '#fff'
+    },
+
+    textos: {
+        textAlignVertical: 'center',
+        fontSize: 15,
+        marginLeft: 10
+    },
+
+    img:{
+        width: 71, 
+        height:71
+    }
+});
+
+  export default styles;
