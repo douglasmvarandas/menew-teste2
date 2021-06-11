82fc659f0d5ce648e1e083c61da7e1bfa6d0c1a6
thomasalves11 <thomas.alves@agisrn.com>
2021-06-11 14:52:54 -0300

    ajustes nas fotos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import { Cards } from '../../components/cards/Cards';
 import { Menu } from '../../components/header/Menu';
 import './Home.css';
-
+import sushi from '../../assets/sushi-boat.jpg'
+import pizza from '../../assets/pizza.jpg'
+import almoco from '../../assets/almoco.jpg'
 
 function Home() {
 
     var itens = [
         {
             nome: "Almoço brasileiro",
-            descricao: "O prato contem: feijão, Arroz, batata frita e file de carne ao molho madeira",
-            foto: "https://portal.fgv.br/sites/portal.fgv.br/files/styles/noticia_geral/public/noticias/set/17/prato-feito.jpg?itok=klqGuxxN&c=33313733cdad61e4bd51beabb4a84531"
+            descricao: "O prato contem: feijão, Arroz e file de carne ao molho madeira",
+            foto: almoco
         },
         {
             nome: "Pizza de calabreza",
             descricao: "Pizza massa fina com calabreza, cebola e azeitonas",
-            foto: "https://blog.novasafra.com.br/wp-content/uploads/2016/01/accb7d_37d37d9f1001e88384dafc6e5cc60c4f.jpg_1024-780x450.jpg"
+            foto: pizza
         },
         {
           nome: "Barca de sushi",
           descricao: "Sashime de Salmão, Sashimi de Atum, Nigiri de Salmão",
-          foto: "../../assets/sushi-boat.jpg"
+          foto: sushi
       }
     ]
 
