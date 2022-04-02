7df4408d986d5ef3792b666dc863c863bc53540f
Rickson Lima <rickson.lima@grupoicts.com.br>
2022-04-01 22:41:51 -0400

    add new strings messages

--- src/constants/strings.constants.ts
+++ src/constants/strings.constants.ts
@@ -1,8 +1,10 @@
 export const SIGNUP_MODAL_TITLE = 'Olá, faça seu cadastro aqui ;)';
-export const LOGIN_MODAL_TITLE = 'Bem-vindo de volta :)';
+export const LOGIN_MODAL_TITLE = 'Faça seu login por aqui :)';
 export const EDIT_PRODUCT_MODAL_TITLE = 'Editar produto';
 export const ERROR_SIGNUP_MESSAGE = 'Todos os campos são obrigatórios.';
 export const SUCESS_SIGNUP_MESSAGE = 'Sucesso! Faça login com suas credencias ;)';
 export const ERROR_LOGIN_MESSAGE = 'Email ou senha incorretos.';
+export const SUCESS_UPDATE_PRODUCT_MESSAGE = 'Produto atualizado com sucesso ;)';
+export const ERROR_UPDATE_PRODUCT_MESSAGE = 'Ops... Todos os campos sáo obrigatórios ;)';
 export const SUCESS_LOGIN_GREETING_MESSAGE = (username: string) =>
   `Bem-vindo de volta, ${username.split(' ')[0]} :)`;
