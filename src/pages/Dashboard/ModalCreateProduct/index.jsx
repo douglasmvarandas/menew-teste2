c09a037852fabd3116cabeae0a02997be4a159b3
VictorOliveira02 <victoralvees17@gmail.com>
2022-04-02 10:53:40 -0300

    organize initial folders

---
+++ src/pages/Dashboard/ModalCreateProduct/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModalCreateProduct = () => {
+    return (
+        <>
+
+        </>
+
+    );
+}
+
+export default ModalCreateProduct;
---
+++ src/pages/Dashboard/ModalEditProduct/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModalEditProduct = () => {
+    return (
+        <>
+
+        </>
+
+    );
+}
+
+export default ModalEditProduct;
