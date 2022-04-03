cb07aa2d295ca845801cee6f48c20cbb384659cc
Pierre Kalil <pierrekalil1@gmail.com>
2022-04-03 12:31:39 -0300

    feat: add userReducer in store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { productReducer } from "./product/reducer"
 import { signinReducer } from "./signin/reducer";
+import { userReducer } from "./user/reducer";
 
 const reducers = combineReducers({
   product: productReducer,
-  signin: signinReducer
+  signin: signinReducer,
+  user: userReducer
 });
 
 const store = createStore(reducers, applyMiddleware(thunk));
